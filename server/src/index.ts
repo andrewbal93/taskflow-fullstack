195f7f0215898bb99da978ba477a1823dca5c3f1
andrewbal93 <andrewbal93@gmail.com>
2026-03-04 11:41:50 +0200

    feat: add prisma and neon db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import rootRouter from './modules/index.routes.js';
+import { prisma } from './lib/prisma.js';
 
 dotenv.config();
 
@@ -13,6 +14,23 @@ app.use(express.json());
 
 app.use('/api', rootRouter);
 
+app.post('/test-user', async (req, res) => {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: 'Test User',
+        email: 'testuser@example.com',
+        password: 'password123',
+      },
+    });
+
+    res.json(user);
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+  });
+  
 app.get('/', (req, res) => {
   res.json('TaskFlow API is running...');
 });
